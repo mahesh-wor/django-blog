8220ea47d8ace065f86093f0ae6485a04a82251a
mahesh-wor <maheshwor89@gmail.com>
2018-11-20 22:34:15 +0545

    added email recovery + Sorting

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render , get_object_or_404
 from django.contrib.auth.mixins import LoginRequiredMixin ,UserPassesTestMixin
 from django.views.generic import (
                                     ListView,
@@ -9,6 +9,7 @@ from django.views.generic import (
 
 )
 from .models import Post
+from django.contrib.auth.models import User
 
 #from django.http import HttpResponse
 # Create your views here.
@@ -25,7 +26,7 @@ from .models import Post
 #            'title':'dotatutorial_noob',
 #        }
 #]
-
+#function based.
 """
 def home(request):
     context = {
@@ -40,6 +41,7 @@ class PostListView(ListView):
     template_name = 'blog/home.html' #<app>/<model>_<viewtype>.html
     context_object_name = 'posts'    #blog/post_detail
     ordering = ['-date_posted']
+    paginate_by = 3
 
 ##DetailView
 class PostDetailView(DetailView):
@@ -79,6 +81,17 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
         return False
 
 
+#For authors only. Sort
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html' #<app>/<model>_<viewtype>.html
+    context_object_name = 'posts'    #blog/post_detail
+    #ordering = ['-date_posted']
+    paginate_by = 3
+
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 
 
 
